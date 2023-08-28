987f9863e1f9ee0599800e1fd962bebffe01dc89
gonzalopozo <gonzalopozo2005@gmail.com>
2023-08-28 20:49:59 +0200

    ¿Qué es el Hoisting? 2

--- src/hoisting/index.js
+++ src/hoisting/index.js
@@ -1,2 +1,18 @@
-console.log(nameOfDog);
-var nameOfDog = 'Elmo';
+
+// var nameOfDog; // undefined
+
+// console.log(nameOfDog);
+// var nameOfDog = 'Elmo';
+// console.log(nameOfDog);
+
+// var elmo; // undefined // Se eleva
+// function nameOfDog() { // Se eleva
+//     console.log(`El mejor perrito es ${elmo}`);
+// }
+nameOfDog();
+
+function nameOfDog() {
+    console.log(`El mejor perrito es ${elmo}`);
+}
+
+var elmo = 'Elmito';
